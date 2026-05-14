ebd9b452c3f6451709f67d189346cc2d2491bdc1
eugenempande <eugene.mpande@gofuseit.com>
2026-05-14 10:41:39 +0200

    Enhance document formatting with A4 landscape layout, color palette, and section styles; disable landscape wrapping for tables

--- landscape-tables.lua
+++ landscape-tables.lua
@@ -1,12 +1,4 @@
--- Wrap tables with 5+ columns in landscape environment
+-- Landscape wrapping disabled: whole document is landscape via header.tex
 function Table(el)
-  local cols = #el.colspecs
-  if cols >= 5 then
-    return {
-      pandoc.RawBlock('latex', '\\begin{landscape}'),
-      el,
-      pandoc.RawBlock('latex', '\\end{landscape}')
-    }
-  end
   return el
 end
